9122a163d1f5404111530cbbbaad6e3c2a609ea2
unknown <grantludwig97@gmail.com>
2019-10-08 22:24:27 -0700

    basic collision

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from graphics import *
 import time
+import math
 
 ballRadius = 20
 
@@ -9,9 +10,9 @@ def main():
     ball1 = Circle(Point(250,250), ballRadius)
     ball1.setFill(color_rgb(130, 0, 130))
     ball1V = [20, 5]
-    ball2 = Circle(Point(300,300), ballRadius)
-    ball1.setFill(color_rgb(130, 0, 130))
-    ball2V = [12, 5]
+    ball2 = Circle(Point(100,100), ballRadius)
+    ball2.setFill(color_rgb(0, 0, 255))
+    ball2V = [-15, -1]
     balls = [ball1, ball2]
     velocities = [ball1V, ball2V]
 
@@ -20,15 +21,38 @@ def main():
 
     for ball in balls:
         ball.draw(win)
+
+    changed = False
     while(True):
+        changed = False
+
         for i in range(0, len(balls)):
             location = balls[i].getCenter()
-            if location.x >= 480 - ballRadius or location.x <= 0 + ballRadius:
+            if location.x >= 480 - ballRadius or location.x <= ballRadius:
                 velocities[i][0] = -velocities[i][0]
-            if location.y >= 640 - ballRadius or location.y <= 0 + ballRadius:
+                changed = True
+            if location.y >= 640 - ballRadius or location.y <= ballRadius:
                 velocities[i][1] = -velocities[i][1]
+                changed = True
 
+        # check collisions
+        if not changed:
+            ball1Loct = balls[0].getCenter()
+            ball2Loct = balls[1].getCenter()
+            if math.sqrt((ball2Loct.x - ball1Loct.x)**2 + (ball2Loct.y - ball1Loct.y)**2) <= (2 * ballRadius):
+                #for i in range(0, len(velocities)):
+                #    velocities[i][0] = -velocities[i][0]
+                #    velocities[i][1] = -velocities[i][1]
+                temp = velocities[0][0]
+                velocities[0][0] = velocities[1][0]
+                velocities[1][0] = temp
+                temp = velocities[0][1]
+                velocities[0][1] = velocities[1][1]
+                velocities[1][1] = temp
+
+        for i in range(0, len(balls)):
             balls[i].move(velocities[i][0], velocities[i][1])
+
         update(30)
     #win.getMouse() # Pause to view result
     win.close()    # Close window when done
