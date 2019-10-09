99447ec66870f2e27558fa9395e0c1b8684ff335
unknown <grantludwig97@gmail.com>
2019-10-09 11:34:10 -0700

    removed old code, changed background

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ ballRadius = 10
 
 def main():
     win = GraphWin("My Circle", 480, 640, autoflush=False)
-    #win.setBackground(color_rgb(0, 0, 0))
+    win.setBackground(color_rgb(0, 0, 0))
     ball1 = Circle(Point(250,250), ballRadius)
     ball1.setFill(color_rgb(130, 0, 130))
     ball1V = [20, 5]
@@ -61,23 +61,6 @@ def main():
                     temp = velocities[i][1]
                     velocities[i][1] = velocities[j][1]
                     velocities[j][1] = temp
-
-        # check collisions
-        #if not changed:
-        #    ball1Loct = balls[0].getCenter()
-        #    ball2Loct = balls[1].getCenter()
-        #    if math.sqrt((ball2Loct.x - ball1Loct.x)**2 + (ball2Loct.y - ball1Loct.y)**2) <= (2 * ballRadius):
-        #        #for i in range(0, len(velocities)):
-        #        #    velocities[i][0] = -velocities[i][0]
-        #        #    velocities[i][1] = -velocities[i][1]
-        #        temp = velocities[0][0]
-        #        velocities[0][0] = velocities[1][0]
-        #        velocities[1][0] = temp
-        #        temp = velocities[0][1]
-        #        velocities[0][1] = velocities[1][1]
-        #        velocities[1][1] = temp
-
-
 
         for i in range(0, len(balls)):
             balls[i].move(velocities[i][0], velocities[i][1])
