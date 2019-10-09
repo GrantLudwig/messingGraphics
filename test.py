d0c2a2da85a7c2b0bf443e363d79174610d5f0f3
unknown <grantludwig97@gmail.com>
2019-10-09 11:33:08 -0700

    collisions working

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from graphics import *
 import time
 import math
 
-ballRadius = 20
+ballRadius = 10
 
 def main():
     win = GraphWin("My Circle", 480, 640, autoflush=False)
@@ -13,8 +13,17 @@ def main():
     ball2 = Circle(Point(100,100), ballRadius)
     ball2.setFill(color_rgb(0, 0, 255))
     ball2V = [-15, -1]
-    balls = [ball1, ball2]
-    velocities = [ball1V, ball2V]
+    ball3 = Circle(Point(400,100), ballRadius)
+    ball3.setFill(color_rgb(0, 255, 0))
+    ball3V = [-25, 10]
+    ball4 = Circle(Point(50,30), ballRadius)
+    ball4.setFill(color_rgb(255, 0, 0))
+    ball4V = [-5, 20]
+    ball5 = Circle(Point(300,600), ballRadius)
+    ball5.setFill(color_rgb(255, 255, 0))
+    ball5V = [5, -15]
+    balls = [ball1, ball2, ball3, ball4, ball5]
+    velocities = [ball1V, ball2V, ball3V, ball4V, ball5V]
 
     border = Rectangle(Point(1,1), Point(479, 639))
     border.draw(win)
@@ -28,27 +37,47 @@ def main():
 
         for i in range(0, len(balls)):
             location = balls[i].getCenter()
-            if location.x >= 480 - ballRadius or location.x <= ballRadius:
-                velocities[i][0] = -velocities[i][0]
+
+            if location.x >= 480 - ballRadius:
+                velocities[i][0] = -abs(velocities[i][0])
                 changed = True
-            if location.y >= 640 - ballRadius or location.y <= ballRadius:
-                velocities[i][1] = -velocities[i][1]
+            elif location.x <= ballRadius:
+                velocities[i][0] = abs(velocities[i][0])
                 changed = True
 
+            if location.y >= 640 - ballRadius:
+                velocities[i][1] = -abs(velocities[i][1])
+                changed = True
+            elif location.y <= ballRadius:
+                velocities[i][1] = abs(velocities[i][1])
+                changed = True
+
+            for j in range(i + 1, len(balls)):
+                ball2Loct = balls[j].getCenter()
+                if math.sqrt((ball2Loct.x - location.x)**2 + (ball2Loct.y - location.y)**2) <= (2 * ballRadius):
+                    temp = velocities[i][0]
+                    velocities[i][0] = velocities[j][0]
+                    velocities[j][0] = temp
+                    temp = velocities[i][1]
+                    velocities[i][1] = velocities[j][1]
+                    velocities[j][1] = temp
+
         # check collisions
-        if not changed:
-            ball1Loct = balls[0].getCenter()
-            ball2Loct = balls[1].getCenter()
-            if math.sqrt((ball2Loct.x - ball1Loct.x)**2 + (ball2Loct.y - ball1Loct.y)**2) <= (2 * ballRadius):
-                #for i in range(0, len(velocities)):
-                #    velocities[i][0] = -velocities[i][0]
-                #    velocities[i][1] = -velocities[i][1]
-                temp = velocities[0][0]
-                velocities[0][0] = velocities[1][0]
-                velocities[1][0] = temp
-                temp = velocities[0][1]
-                velocities[0][1] = velocities[1][1]
-                velocities[1][1] = temp
+        #if not changed:
+        #    ball1Loct = balls[0].getCenter()
+        #    ball2Loct = balls[1].getCenter()
+        #    if math.sqrt((ball2Loct.x - ball1Loct.x)**2 + (ball2Loct.y - ball1Loct.y)**2) <= (2 * ballRadius):
+        #        #for i in range(0, len(velocities)):
+        #        #    velocities[i][0] = -velocities[i][0]
+        #        #    velocities[i][1] = -velocities[i][1]
+        #        temp = velocities[0][0]
+        #        velocities[0][0] = velocities[1][0]
+        #        velocities[1][0] = temp
+        #        temp = velocities[0][1]
+        #        velocities[0][1] = velocities[1][1]
+        #        velocities[1][1] = temp
+
+
 
         for i in range(0, len(balls)):
             balls[i].move(velocities[i][0], velocities[i][1])
