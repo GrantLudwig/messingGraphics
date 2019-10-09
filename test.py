b95b17919273a841764a7c46e4ce2a5b0b87d2df
GrantLudwig <grantludwig97@gmail.com>
2019-10-08 18:59:11 -0700

    changes

--- test.py
+++ test.py
@@ -1,14 +1,35 @@
 from graphics import *
 import time
 
+ballRadius = 20
+
 def main():
-    win = GraphWin("My Circle", 480, 640)
-    c = Circle(Point(250,250), 10)
-    c.setFill(color_rgb(130, 0, 130))
-    c.draw(win)
+    win = GraphWin("My Circle", 480, 640, autoflush=False)
+    #win.setBackground(color_rgb(0, 0, 0))
+    ball1 = Circle(Point(250,250), ballRadius)
+    ball1.setFill(color_rgb(130, 0, 130))
+    ball1V = [20, 5]
+    ball2 = Circle(Point(300,300), ballRadius)
+    ball1.setFill(color_rgb(130, 0, 130))
+    ball2V = [12, 5]
+    balls = [ball1, ball2]
+    velocities = [ball1V, ball2V]
+
+    border = Rectangle(Point(1,1), Point(479, 639))
+    border.draw(win)
+
+    for ball in balls:
+        ball.draw(win)
     while(True):
-        c.move(10, 0)
-        update(90)
+        for i in range(0, len(balls)):
+            location = balls[i].getCenter()
+            if location.x >= 480 - ballRadius or location.x <= 0 + ballRadius:
+                velocities[i][0] = -velocities[i][0]
+            if location.y >= 640 - ballRadius or location.y <= 0 + ballRadius:
+                velocities[i][1] = -velocities[i][1]
+
+            balls[i].move(velocities[i][0], velocities[i][1])
+        update(30)
     #win.getMouse() # Pause to view result
     win.close()    # Close window when done
 
